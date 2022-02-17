553f54b5b42b7d81c820dc7a99726e65e61f1241
Neil Massey <neil.massey@stfc.ac.uk>
2022-02-17 12:32:27 +0000

    Version using minio, gets all the objects in a bucket

--- get_keys.py
+++ get_keys.py
@@ -1,6 +1,5 @@
-
-import xml.etree.ElementTree as ET
-import urllib.request
+from minio import Minio
+from urllib.parse import urlparse, urljoin
 import time
 import os
 import sys
@@ -9,22 +8,29 @@ def get_bucketlist(url):
     """Get the listing from the object store and return a plain list of keys.
        Keys look like filenames."""
 
-    # do some cacheing for the listing file
-    cachefile = os.path.join("/tmp", os.path.basename(url) + ".xml")
-    if not os.path.exists(cachefile) or os.path.getmtime(cachefile) - time.time() > 1000:
-        with urllib.request.urlopen(url) as f:
-            content = f.read()
-        with open(cachefile, "wb") as f:
-            f.write(content)
+    # do some cacheing for the listing file - cache time in seconds
+    cachefile = os.path.join("/tmp", os.path.basename(url) + ".txt")
+    if (not os.path.exists(cachefile) or 
+        (time.time() - os.path.getmtime(cachefile) > 1000)):
 
-    # extract the keys into a plain list
-    tree = ET.parse(cachefile)
-    root = tree.getroot()
-    blist = []
-    for key in root.findall('{http://s3.amazonaws.com/doc/2006-03-01/}Contents/{http://s3.amazonaws.com/doc/2006-03-01/}Key'):
-        blist.append(key.text)
+        # first parse the url to get the server address
+        urlp = urlparse(url)
+        # connect using Minio client, anonymously
+        mc = Minio(urlp.netloc, secure=False)
+        # get the objects in the bucket
+        bl = mc.list_objects(urlp.path[1:], recursive=True)  # trim the /
+        with open(cachefile, "w") as f:
+            for obj in bl:
+                # obj_name = (
+                #     urlp.scheme + "://" + urlp.netloc + "/" + obj.object_name
+                # )
+                obj_name = obj.object_name
+                f.write(obj_name + "\n")
+
+    # cache file written, open and read as a list
+    with open(cachefile, "r") as f:
+        blist = f.read().split("\n")
     return blist
-
                              
 if  __name__ == "__main__":
     # print a list of urls that you could grab the data from ...
